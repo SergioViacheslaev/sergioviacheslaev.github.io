f075d50d85333b52298629b77a63c665134d4caf
SergioViacheslaev <internetwalker@yandex.ru>
2021-08-04 18:35:02 +0300

    Refactor profile-card

--- src/components/profile-card/profile-card.component.jsx
+++ src/components/profile-card/profile-card.component.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {Grid, GridItem, Image, Text} from '@chakra-ui/react';
 import Typist from 'react-typist';
+import {Grid, GridItem, Image, List, ListIcon, ListItem} from '@chakra-ui/react';
+import {MdCheckCircle} from "react-icons/all";
 
 import profilePic from '../../assets/developer_avatar.jpg';
 
@@ -16,49 +17,61 @@ const ProfileCard = () => {
             templateColumns={{base: '1fr', md: '2fr 2fr', lg: '1fr 3fr'}}
             mt={10}
             p={4}
+            paddingBottom={20}
             gridColumnGap={10}
             textAlign={{base: 'center', md: 'inherit'}}>
             <GridItem alignSelf="center">
                 <Image src={profilePic} borderRadius="full" alt="Sergei Viacheslaev"/>
             </GridItem>
             <GridItem paddingTop="1px" alignSelf="top">
-                {/*<Text fontSize="2xl" fontWeight="bold">*/}
-                {/*    Hello !*/}
-                {/*</Text>*/}
                 {count ? (<Typist cursor={{show: false, hideWhenDone: true}}
                                   onTypingDone={() => setCount(0)}>
             <span style={{fontSize: '24px', fontWeight: 'bold', color: '#1da1f2'}}>
               Hello and welcome !
               <br/>
               I am {' '}
-                <span style={{fontSize: '24px', fontWeight: 'bold', color: '#00BFFF'}}>
+                <span style={{fontSize: '24px', fontWeight: 'bold', color: '#FF8C00'}}>
                 Sergei Viacheslaev
                 </span>
-                {/*<Typist.Delay ms={99999999}/>*/}
-                    <Typist.Backspace count={18} delay={2500} />
+                <Typist.Backspace count={18} delay={2500}/>
+                   <span style={{fontSize: '24px', fontWeight: 'bold', color: '#FF8C00'}}>
+                   Java {''}
+                   </span>
+                   developer
+                <Typist.Backspace count={14} delay={2500}/>
+                  <span style={{fontSize: '24px', fontWeight: 'bold', color: '#FF8C00'}}>
+                    Kotlin {''}
+                  </span>
+                    developer
+                 <Typist.Backspace count={16} delay={2500}/>
+                    interested in {''}
                 <span style={{fontSize: '24px', fontWeight: 'bold', color: '#FF8C00'}}>
-                Java developer
-                </span>
-
-                    <Typist.Backspace count={14} delay={2500} />
-                    {/*Software engineer*/}
-                    {/*<Typist.Backspace count={14} delay={2500} />*/}
-                    {/*IT specialist*/}
-                    {/*<Typist.Backspace count={18} delay={6000} />*/}
+                    Full-stack {''}
+                  </span>
+                    development
+                 <Typist.Backspace count={36} delay={900000000}/>
             </span>
-                    </Typist>
-                ) : (
-                    ''
-                )}
+                </Typist>) : ('')}
                 <br/>
-                <Text fontSize="lg">
-                    Under construction
-                </Text>
+                <List spacing={3}>
+                    <ListItem>
+                        <ListIcon as={MdCheckCircle} color="green.500"/>
+                        Oracle Certified Java 11 Professional
+                    </ListItem>
+                    <ListItem>
+                        <ListIcon as={MdCheckCircle} color="green.500"/>
+                        Hands-on experience building enterprise Web applications
+                    </ListItem>
+                    <ListItem>
+                        <ListIcon as={MdCheckCircle} color="green.500"/>
+                        IT background: information security, network engineer, system administration
+                    </ListItem>
+                    <ListItem>
+                        <ListIcon as={MdCheckCircle} color="green.500"/>
+                        Creator of the free online course "How to write Telegram-bots with Java + Spring"
+                    </ListItem>
+                </List>
                 <br/>
-                {/*todo: add text*/}
-                <Text fontSize="lg">
-
-                </Text>
             </GridItem>
         </Grid>
     );
