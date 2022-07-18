2ef06badd13ef0ddf9145b0836523d03c9414a07
SergioViacheslaev <internetwalker@yandex.ru>
2022-07-18 22:50:31 +0300

    Refactor CV link

--- src/components/nav-item/nav-item.component.jsx
+++ src/components/nav-item/nav-item.component.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
-import { Button } from '@chakra-ui/button';
-import { Link, withRouter } from 'react-router-dom';
-import { FaExternalLinkAlt } from 'react-icons/fa';
+import {Button} from '@chakra-ui/button';
+import {Link, withRouter} from 'react-router-dom';
 
-const NavItem = ({ itemName, routeUrl, location }) => {
-  return itemName !== 'CV' ? (
-    <Link to={routeUrl}>
-      <Button
-        marginLeft={{ base: '6px', md: '12px' }}
-        variant={location.pathname === routeUrl ? 'outline' : 'solid'}
-      >
-        {itemName}
-      </Button>
-    </Link>
-  ) : (
-    <Button
-      as="a"
-      marginLeft={{ base: '6px', md: '12px' }}
-      href={routeUrl}
-      target="_blank"
-      rel="noreferrer"
-      rightIcon={<FaExternalLinkAlt />}
-    >
-      {itemName}
-    </Button>
-  );
+const NavItem = ({itemName, routeUrl, location}) => {
+    //todo: uncomment after CV is ready
+
+    /*  return itemName !== 'CV' ? (
+        <Link to={routeUrl}>
+          <Button
+            marginLeft={{ base: '6px', md: '12px' }}
+            variant={location.pathname === routeUrl ? 'outline' : 'solid'}
+          >
+            {itemName}
+          </Button>
+        </Link>
+      ) : (
+        <Button
+          as="a"
+          marginLeft={{ base: '6px', md: '12px' }}
+          href={routeUrl}
+          target="_blank"
+          rel="noreferrer"
+          rightIcon={<FaExternalLinkAlt />}
+        >
+          {itemName}
+        </Button>
+      );*/
+    return (
+        <Link to={routeUrl}>
+            <Button
+                marginLeft={{base: '6px', md: '12px'}}
+                variant={location.pathname === routeUrl ? 'outline' : 'solid'}
+            >
+                {itemName}
+            </Button>
+        </Link>
+    );
 };
 
 export default withRouter(NavItem);
